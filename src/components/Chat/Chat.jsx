0e54579caa85e538d7d3a5083c4f1e75284b6118
navdeepsingh4298 <navdeepsingh4298@gmail.com>
2021-07-18 00:31:06 +0530

    add chatengine into chat component

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,16 +1,36 @@
 import React, { useEffect } from 'react';
 import { useChat } from 'context';
+import { getChats, ChatEngine } from 'react-chat-engine';
 
 export const Chat = () => {
-  const {chatConfig} = useChat();
+  const {myChats, setMyChats, chatConfig, selectedChat } = useChat();
 
   useEffect(() => {
-    console.log(chatConfig);
-  },[chatConfig]);
+    console.log("my Chats: ", myChats);
+  },[myChats]);
 
   return (
-    <div>
-      This is Chat
-    </div>
+    <>
+      {!!chatConfig && (
+        <ChatEngine
+          hideUI={true}
+          userName={chatConfig.userName}
+          projectId={chatConfig.projectId}
+          userSecret={chatConfig.userSecret}
+          onConnect={() => {
+            getChats(chatConfig, setMyChats);
+          }}
+        />
+      )}
+      <div className="chat-container">
+        <div className="current-chat">
+          {selectedChat ? <></> :
+            <div className="no-chat-selected">
+              <span className="point-left">👈</span>
+              <span>Select a Chat</span>
+          </div>}
+        </div>
+      </div>
+    </>
   )
 }
