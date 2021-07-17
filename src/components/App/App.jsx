5faccf5df0c57732cad89dbfbcb60b66079cffa5
navdeepsingh4298 <navdeepsingh4298@gmail.com>
2021-07-17 23:59:22 +0530

    add chat context and useChat custom hook

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,7 @@ import {useEffect} from "react";
 import { Route, Switch, useHistory } from "react-router-dom";
 import { Login, Signup, Chat } from "components";
 import { useAuth, useResolved } from "hooks";
+import { ChatProvider } from 'context';
 
 export const App = () => {
   const history = useHistory();
@@ -14,17 +15,19 @@ export const App = () => {
     }
   }, [authUser, authResolved, history]);
 
-  return (
-    <>
+  return authResolved ? (
+    <ChatProvider authUser={authUser}>
       <div className="app">
         <Switch>
-        <Route exact path="/" component={Chat} />
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={Signup} />
-      </Switch> 
+          <Route exact path="/" component={Chat} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+        </Switch>
       </div>
-    </>
-  )
+    </ChatProvider>
+  ) : (
+    <> Loading... </>
+  );
 }
 
 
--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useChat } from 'context';
 
 export const Chat = () => {
+  const {chatConfig} = useChat();
+
+  useEffect(() => {
+    console.log(chatConfig);
+  },[chatConfig]);
+
   return (
     <div>
       This is Chat
